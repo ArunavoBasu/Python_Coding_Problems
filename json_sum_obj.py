fbe90931d871db2c5db4df7c3b5f0f99ffdffcd2
unknown <arunavobasu.ds@gmail.com>
2025-06-16 08:03:22 +0530

    change in the json_sum_obj file

--- json_sum_obj.py
+++ json_sum_obj.py
@@ -1,6 +1,6 @@
 ### There will be a list of JSON objects such as - [{"a": 3, "b": 2, "c": 5}, {"a": 2, "b": 3}, {"c": 5}]. The output will be a JSON object as - {"a": 5, "b": 5, "c": 10}
-
-def json_sum(my_arr: list) -> object:
+from typing import List
+def json_sum(my_arr: List) -> object:
     freq_json = {}
     for obj in my_arr:
         for keys, values in obj.items():
