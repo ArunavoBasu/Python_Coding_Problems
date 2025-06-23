9d908b39ce3e6a2a0eceed1b073f3cdd549610d9
unknown <arunavobasu.ds@gmail.com>
2025-06-23 08:27:35 +0530

    Added the lonely_integer.py file

---
+++ lonely_integer.py
@@ -0,0 +1,16 @@
+from typing import List
+def lonelyinteger(a : List) -> int:
+    seen = {}
+    for num in a:
+        if num in seen.keys():
+            seen[num] += 1
+        else:
+            seen[num] = 1
+ 
+    for key in seen:
+        if seen[key] == 1:
+            return key
+
+
+
+print(lonelyinteger([1,2,3,4,3,2,1]))
